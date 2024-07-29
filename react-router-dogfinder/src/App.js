a1574d230a229524fbfdf04d1507a53b199d443a
Kate Vong <katiejnete@gmail.com>
2024-07-29 15:08:35 -0700

    add styles

--- react-router-dogfinder/src/App.js
+++ react-router-dogfinder/src/App.js
@@ -1,27 +1,30 @@
 import "./App.css";
 import React from "react";
 import Nav from "./Nav";
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from "react-router-dom";
 import DogList from "./DogList";
 import DogDetails from "./DogDetails";
+import dogData from "./dogData";
 
-function App({ defaultProps }) {
-  const {dogs} = defaultProps;
+function App() {
+  const { dogs } = dogData;
+
   return (
     <>
       <Nav dogs={dogs} />
       <Routes>
-        <Route exact path="/dogs" element={<DogList dogs={dogs}/>} />
-        {dogs.map(dog => (<Route 
-          key={dog.name}
-          path={`/dogs/${dog.name}`}
-          element={<DogDetails dog={dog} />}
+        <Route exact path="/dogs" element={<DogList dogs={dogs} />} />
+        {dogs.map((dog) => (
+          <Route
+            key={dog.name}
+            path={`/dogs/${dog.name}`}
+            element={<DogDetails dog={dog} />}
           />
         ))}
-        <Route path="*" element={<Navigate to="/dogs"/>} />
+        <Route path="*" element={<Navigate to="/dogs" />} />
       </Routes>
     </>
   );
-};
+}
 
 export default App;
